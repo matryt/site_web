cdcc3fbcc91d66c91fdb3a3f77b5369d9eff924a
matryt <matryt@users.noreply.github.com>
2021-01-06 20:44:02 +0100

    Réorganisation et création fct info

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    window.onscroll = function (ev) {
-        document.getElementById("Haut_page").className = (window.pageYOffset > 100) ? "visible" :
-            "invisible";
-    };
+document.addEventListener("DOMContentLoaded", function () {
+	window.onscroll = function (ev) {
+		document.getElementById("Haut_page").className =
+			window.pageYOffset > 100 ? "visible" : "invisible";
+	};
 });
 
 var today = new Date();
 var annee = today.getFullYear();
-var sannee = document.getElementById('annee');
+var sannee = document.getElementById("annee");
 sannee.innerHTML = annee;
+
+setTimeout(info, 2000)
+
+function info() {
+    alert(
+        "Pour consulter la définition d'un terme que vous ne comprendriez pas et qui est en vert, veuillez cliquer sur le terme en question." )
+}
