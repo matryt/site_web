8f0e43e292840811e330beb3f5234af3aecc9723
matryt <matryt@users.noreply.github.com>
2021-01-11 09:47:41 +0100

    Insertion fct info

--- script/script.js
+++ script/script.js
@@ -16,3 +16,14 @@ function info() {
     alert(
         "Pour consulter la définition d'un terme que vous ne comprendriez pas et qui est en vert, veuillez cliquer sur le terme en question." )
 }
+
+function langagemachine () {
+    alert("Le langage machine, ou code machine, est la suite de bits qui est interprétée par le processeur d'un ordinateur exécutant un programme informatique. C'est le langage natif d'un processeur, c'est-à-dire le seul qu'il puisse traiter. Il est composé d'instructions et de données à traiter codées en binaire.");
+    return False;
+}
+
+function veriftype() {
+    alert("Le contrôle de type vérifie qu'une opération apparaissant dans une instruction porte bien sur des valeurs adéquates.");
+    return False;
+}
+
